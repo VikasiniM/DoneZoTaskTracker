f1f95dd744b9fa22a6781dde1868a257e04e9661
VikasiniM <vikasiniias@gmail.com>
2025-04-05 09:31:55 +0530

    Initial commit for DoneZoTaskTracker

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/taskbuddy/",  // Replace with your GitHub repo name
-});
+  base: '/DoneZoTaskTracker/'// ✅ THIS IS IMPORTANT
+})
